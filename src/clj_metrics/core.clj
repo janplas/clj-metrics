304871fba899b26ec0e58ce582ef4ba4d0ca2adc
Maurits <maurits.rijk@gmail.com>
2011-04-27 18:31:34 +0200

    Minor refactoring

--- src/clj_metrics/core.clj
+++ src/clj_metrics/core.clj
@@ -1,7 +1,6 @@
 (ns clj-metrics.core
   (:require [clojure.string :as str]
-            [clojure.contrib.string :as str-utils]
-            [clojure.walk :as walk])
+            [clojure.contrib.string :as str-utils])
   (:import (java.io File)))
 
 (defn read-source
@@ -32,7 +31,8 @@
 (defn- create-source-info
   "Create map with source info"
   [file]
-  {:src (read-source file)})
+  (let [path (.getPath file)]
+    {:src (read-source file) :path path}))
 
 (defn read-all-clj-files [dir]
   (map create-source-info (get-all-clj-files dir)))
@@ -40,15 +40,23 @@
 (defn get-nr-of-lines [seq]
   (map #(nr-of-lines (% :src)) seq))
 
+(defn get-paths [seq]
+  "Returns all path names"
+  (map :path seq))
+
 (defn- create-ast
   "Create a kind of abstract syntax tree"
   [source]
   (read-string (str "(" source ")")))
 
+(defn nr-of-toplevel-symbols
+  [ast symbol-name]
+  (count (filter #(= (symbol symbol-name) (first %)) ast)))
+
 (defn nr-of-defns
   "Count functions in file" 
   [ast]
-  (count (filter #(= (symbol "defn") (first %)) ast)))
+  (nr-of-toplevel-symbols ast "defn"))
 
 (defn get-nr-of-defns [seq]
   (map #(nr-of-defns (create-ast (% :src))) seq))
@@ -56,5 +64,6 @@
 (defn -main [& [args]]
   (let [dir (if args args ".")
         clj-files (read-all-clj-files dir)]
+    (println (get-paths clj-files))
     (println (get-nr-of-lines clj-files))
     (println (get-nr-of-defns clj-files))))
