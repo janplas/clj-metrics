50763d80755dea61ffb7122fb0f68a6572f4d27d
Maurits <maurits.rijk@gmail.com>
2011-05-03 19:06:12 +0200

    Added couple of tests

--- test/clj_metrics/test/defn.clj
+++ test/clj_metrics/test/defn.clj
@@ -4,9 +4,15 @@
 
 (def *tree1* '(defn foo "This is foo!" [x] (* x x)))
 (def *tree2* '(defmacro foo "This is foo!" [x] (* x x)))
+(def *tree3* '(defn foo [x] (* x x)))
 
 (deftest test-is-defn?
   (is (is-defn? *tree1*))
   (is (not (is-defn? *tree2*))))
 
+(deftest test-get-comment
+  (is (= (get-comment *tree1*) "This is foo!")))
 
+(deftest test-has-comment
+  (is (has-comment? *tree1*))
+  (is (not (has-comment? *tree3*))))
