ad4801eb30b6f72f4f6d043e1a479a5795a26a09
Maurits <maurits.rijk@gmail.com>
2011-05-06 20:16:09 +0200

    Removed unused code

--- src/clj_metrics/defn.clj
+++ src/clj_metrics/defn.clj
@@ -6,18 +6,6 @@
   [decl]
   (= (symbol "defn") (first decl)))
 
-; fix me: we probably don't want to call eval every time we need something from a
-; function
-(defn get-comment
-  "Get documentation string"
-  [func]
-  (-> func eval meta :doc))
-
-(defn has-comment
-  "Returns true if func has a comment string, otherwise false"
-  [func]
-  (not (str-utils/blank? (get-comment func))))
-
 (defn has-comment?
   "Returns true if func has a comment string, otherwise false"
   [func]
--- src/clj_metrics/util.clj
+++ src/clj_metrics/util.clj
@@ -3,14 +3,9 @@
 (def count-if (comp count filter))
 
 (defn merge-seq [& args]
+  "Merge number of labeled sequences into a list of maps"
   (let [keys (map first args)
         seqs (map second args)]
     (apply map (fn [& args] (zipmap keys args)) seqs)))
-
-; Fix me: next func should be generalised
-(defn merge-seq2
-  "Merge 2 sequences into list of maps with given labels"
-  [seq1 label1 seq2 label2]
-  (map (fn [x y] {(keyword label1) x (keyword label2) y}) seq1 seq2))
   
   
